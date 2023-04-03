25f71877f10f268b1f24de5d4bf72ca58aa126ef
HOUDA LALAOUI MOUTAOUAKKIL <houdalalaoui115@gmail.com>
2023-04-03 12:58:19 +0000

    testing with junit 4 kotlin

---
+++ src/main/kotlin/Ex1UnitTest.kt
@@ -0,0 +1,25 @@
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class Ex1UnitTest {
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        println("Before ")
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        println("After ")
+    }
+
+    @Test
+    fun equalString() {
+        println("Je developpeur Mobile")
+        val str1 = "Je developpeur Mobile"
+        assertEquals("Je developpeur Mobile", str1)
+    }
+}
